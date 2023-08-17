c73518ebf0e5002c16b39948f877b547966bba11
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-08-17 20:16:24 +0300

    news section main page

--- frontend/src/components/Main/News/index.tsx
+++ frontend/src/components/Main/News/index.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import styles from './News.module.scss';
+import { Link } from 'react-router-dom';
+import news from './news.json';
 
 const News = () => {
-  return <div>News</div>;
+  return (
+    <section className={styles.news}>
+      <h2 className={styles.news__title}>Последние новости</h2>
+      <div className={styles.news__linkMore}>
+        <Link to='/news'>Посмотреть все</Link>
+      </div>
+      <div className={styles.news__content}>
+        {news.map((article) => (
+          <div
+            className={styles.news__article}
+            style={{
+              background: `url("${article.img}")`,
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center center',
+              backgroundSize: 'cover',
+            }}>
+            <div className={styles.news__article__overlay}>
+              <span className={styles.news__article__date}>{article.date}</span>
+              <h4 className={styles.news__article__title}>{article.title}</h4>
+              <p className={styles.news__article__text}>{article.text}</p>
+              <p className={styles.news__article__endword}>Приятного просмотра!</p>
+              <Link to='/news' className={styles.news__article__button}>
+                Подробнее
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default News;
