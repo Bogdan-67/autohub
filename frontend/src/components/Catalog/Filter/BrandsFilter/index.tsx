a935598738de9335b73dbc54b4b05e190b2475e1
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-08-21 23:10:27 +0300

    BrandsFilter placed in FilterBlock

--- frontend/src/components/Catalog/Filter/BrandsFilter/index.tsx
+++ frontend/src/components/Catalog/Filter/BrandsFilter/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useAppDispatch } from '../../../../redux/store';
 import styles from './BrandsFilter.module.scss';
-import { LiaAngleRightSolid } from 'react-icons/lia';
 import classNames from 'classnames';
 import { useSelector } from 'react-redux';
 import {
@@ -10,8 +9,8 @@ import {
   clearBrands,
   removeBrand,
 } from '../../../../redux/slices/filterSlice';
-import { RxCross2 } from 'react-icons/rx';
 import brands from '../../../Main/BrandsSlider/brands.json';
+import FilterBlock from '../FilterBlock';
 
 const BrandsFilter = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -27,35 +26,23 @@ const BrandsFilter = () => {
   };
 
   return (
-    <div className={styles.brands}>
-      <div className={styles.brands__header} onClick={() => setIsOpen(!isOpen)}>
-        <LiaAngleRightSolid
-          className={classNames(styles.brands__header__angle, { [styles.rotated]: isOpen })}
-        />
-        <h5 className={styles.brands__header__title}>Брэнды</h5>
-        {selectedBrands.length > 0 && (
-          <RxCross2
-            className={styles.brands__header__cross}
-            onClick={() => dispatch(clearBrands())}
-            title='Очистить'
-          />
-        )}
-      </div>
-      {isOpen && (
-        <ul className={styles.brands__list}>
-          {brands.map((brand) => (
-            <li
-              key={brand.id}
-              className={classNames(styles.brand, {
-                [styles.active]: selectedBrands.includes(brand.id),
-              })}
-              onClick={() => handleClick(brand.id)}>
-              {brand.name}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <FilterBlock
+      clearFunc={() => dispatch(clearBrands())}
+      title={'Брэнды'}
+      isSelected={!!selectedBrands.length}>
+      <ul className={styles.brands__list}>
+        {brands.map((brand) => (
+          <li
+            key={brand.id}
+            className={classNames(styles.brand, {
+              [styles.active]: selectedBrands.includes(brand.id),
+            })}
+            onClick={() => handleClick(brand.id)}>
+            {brand.name}
+          </li>
+        ))}
+      </ul>
+    </FilterBlock>
   );
 };
 
