7258a62782c66955ae6e0b6a63be82946aa4458e
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-08-21 03:02:39 +0300

    add and remove brand on click

--- frontend/src/components/Catalog/Filter/BrandsFilter/index.tsx
+++ frontend/src/components/Catalog/Filter/BrandsFilter/index.tsx
@@ -4,7 +4,12 @@ import styles from './BrandsFilter.module.scss';
 import { LiaAngleRightSolid } from 'react-icons/lia';
 import classNames from 'classnames';
 import { useSelector } from 'react-redux';
-import { SelectBrands, clearBrands } from '../../../../redux/slices/filterSlice';
+import {
+  SelectBrands,
+  addBrand,
+  clearBrands,
+  removeBrand,
+} from '../../../../redux/slices/filterSlice';
 import { RxCross2 } from 'react-icons/rx';
 import brands from '../../../Main/BrandsSlider/brands.json';
 
@@ -13,6 +18,14 @@ const BrandsFilter = () => {
   const selectedBrands = useSelector(SelectBrands);
   const dispatch = useAppDispatch();
 
+  const handleClick = (id: number) => {
+    if (selectedBrands.includes(id)) {
+      dispatch(removeBrand(id));
+    } else {
+      dispatch(addBrand(id));
+    }
+  };
+
   return (
     <div className={styles.brands}>
       <div className={styles.brands__header} onClick={() => setIsOpen(!isOpen)}>
@@ -20,17 +33,25 @@ const BrandsFilter = () => {
           className={classNames(styles.brands__header__angle, { [styles.rotated]: isOpen })}
         />
         <h5 className={styles.brands__header__title}>Брэнды</h5>
-        {selectedBrands && (
+        {selectedBrands.length > 0 && (
           <RxCross2
             className={styles.brands__header__cross}
             onClick={() => dispatch(clearBrands())}
+            title='Очистить'
           />
         )}
       </div>
       {isOpen && (
         <ul className={styles.brands__list}>
           {brands.map((brand) => (
-            <li>{brand.name}</li>
+            <li
+              key={brand.id}
+              className={classNames(styles.brand, {
+                [styles.active]: selectedBrands.includes(brand.id),
+              })}
+              onClick={() => handleClick(brand.id)}>
+              {brand.name}
+            </li>
           ))}
         </ul>
       )}
