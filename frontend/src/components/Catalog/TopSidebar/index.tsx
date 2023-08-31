b8d2378fae0548066c340f616008dace3fbe5b90
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-08-31 22:14:09 +0300

    add view and compare in TopSidebar

--- frontend/src/components/Catalog/TopSidebar/index.tsx
+++ frontend/src/components/Catalog/TopSidebar/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import Sort from '../Sort';
 import styles from './TopSidebar.module.scss';
+import { useAppSelector } from '../../../hooks/redux';
+import { SelectView, setView } from '../../../redux/slices/filterSlice';
+import { useAppDispatch } from '../../../redux/store';
+import classNames from 'classnames';
 
 const TopSidebar = () => {
+  const view = useAppSelector(SelectView);
+
+  const dispatch = useAppDispatch();
+
   return (
     <div className={styles.content}>
       <Sort />
+      <div className={styles.content__right}>
+        <div className={styles.cards} onClick={() => dispatch(setView('cards'))}>
+          {[...new Array(4)].map((_, index) => (
+            <span key={index} className={classNames({ [styles.active]: view === 'cards' })}></span>
+          ))}
+        </div>
+        <div className={styles.rows} onClick={() => dispatch(setView('rows'))}>
+          {[...new Array(2)].map((_, index) => (
+            <span key={index} className={classNames({ [styles.active]: view === 'rows' })}></span>
+          ))}
+        </div>
+        <div className={styles.compare}>
+          Сравнение: <span className={styles.compare__count}>2</span>
+        </div>
+      </div>
     </div>
   );
 };
