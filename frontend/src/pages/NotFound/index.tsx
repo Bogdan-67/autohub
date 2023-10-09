73fc3c54a5d5cd48349c1f2f24275bc58d301845
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-10-10 01:59:10 +0300

    go back btn on 404 page

--- frontend/src/pages/NotFound/index.tsx
+++ frontend/src/pages/NotFound/index.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import styles from './NotFound.module.scss';
 import Button from '../../components/common/Button';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { AiOutlineHome } from 'react-icons/ai';
 
 type Props = {
   link?: string;
 };
 
 export const NotFound: React.FC<Props> = ({ link }) => {
+  const navigate = useNavigate();
   return (
     <div className={styles.notFound}>
       <span className={styles.notFound__numbers}>404</span>{' '}
       <p className={styles.notFound__text}>Страница не найдена</p>
-      <Link className={styles.notFound__button} to={link ? link : '/'}>
-        <Button>← &nbsp; На главную</Button>
-      </Link>
+      <div className={styles.notFound__buttons}>
+        {window.history.length > 1 && <Button onClick={() => navigate(-1)}>← &nbsp; Назад</Button>}
+        <Link className={styles.notFound__button} to={link ? link : '/'}>
+          <Button>
+            <AiOutlineHome className={styles.notFound__button__icon} /> &nbsp; На главную
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
