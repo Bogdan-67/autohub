9d1691569a97c84b890266e81cd4b58a1d216e6b
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-10-10 01:30:53 +0300

    NotFound page

--- frontend/src/pages/NotFound/index.tsx
+++ frontend/src/pages/NotFound/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import styles from './NotFound.module.scss';
+import Button from '../../components/common/Button';
+import { Link, useLocation } from 'react-router-dom';
 
-export const NotFound: React.FC = () => {
-  return <div>404 Страница не найдена</div>;
+type Props = {
+  link?: string;
 };
+
+export const NotFound: React.FC<Props> = ({ link }) => {
+  return (
+    <div className={styles.notFound}>
+      <span className={styles.notFound__numbers}>404</span>{' '}
+      <p className={styles.notFound__text}>Страница не найдена</p>
+      <Link className={styles.notFound__button} to={link ? link : '/'}>
+        <Button>← &nbsp; На главную</Button>
+      </Link>
+    </div>
+  );
+};
+
 export default NotFound;
