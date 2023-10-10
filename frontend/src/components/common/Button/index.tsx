1228234f78ebb7c75851f733b6f128ecebd3ce0c
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-10-10 21:35:17 +0300

    enable add classNames in button component

--- frontend/src/components/common/Button/index.tsx
+++ frontend/src/components/common/Button/index.tsx
@@ -1,10 +1,15 @@
 import { FC } from 'react';
 import { ButtonProps } from './button.props';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
+import classNames from 'classnames';
 
-const Button: FC<ButtonProps> = ({ loading, ref, children, ...props }) => {
+const Button: FC<ButtonProps> = ({ loading, ref, children, className, ...props }) => {
   return (
-    <button ref={ref} className='button' disabled={loading || props.disabled} {...props}>
+    <button
+      ref={ref}
+      className={classNames('button', className)}
+      disabled={loading || props.disabled}
+      {...props}>
       {loading ? <LoadingSpinner size={17} /> : <>{children}</>}
     </button>
   );
