64d7d3eb558b3fb4c27816c756a8e28415102d11
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-10-10 22:34:34 +0300

    prop color in LoadingSpinner

--- frontend/src/components/common/LoadingSpinner/LoadingSpinner.props.ts
+++ frontend/src/components/common/LoadingSpinner/LoadingSpinner.props.ts
@@ -2,5 +2,6 @@ import { SVGAttributes } from 'react';
 
 export interface Props extends SVGAttributes<HTMLOrSVGElement> {
   size: number;
+  color?: string;
   padding?: number;
 }
--- frontend/src/components/common/LoadingSpinner/LoadingSpinner.tsx
+++ frontend/src/components/common/LoadingSpinner/LoadingSpinner.tsx
@@ -1,14 +1,21 @@
 import { FC } from 'react';
 import { Props } from './LoadingSpinner.props';
 
-const LoadingSpinner: FC<Props> = ({ size, padding, ...props }) => {
+const LoadingSpinner: FC<Props> = ({ size, color, padding, ...props }) => {
   return (
     <svg
       className='spinner'
       style={{ width: `${size}px`, height: `${size}px`, padding: padding ? padding : '0' }}
       viewBox='0 0 50 50'
       {...props}>
-      <circle className='path' cx='25' cy='25' r='20' fill='none' strokeWidth='5'></circle>
+      <circle
+        className='path'
+        style={{ stroke: color ? color : 'currentColor' }}
+        cx='25'
+        cy='25'
+        r='20'
+        fill='none'
+        strokeWidth='5'></circle>
     </svg>
   );
 };
