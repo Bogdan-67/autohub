243ebd7aa68f81307a27fc68d97cc8debbda7213
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-10-10 22:34:55 +0300

    spinnerColor in button

--- frontend/src/components/common/Button/button.props.ts
+++ frontend/src/components/common/Button/button.props.ts
@@ -3,5 +3,6 @@ import { ButtonHTMLAttributes, MutableRefObject } from 'react';
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   loading?: boolean;
   ref?: MutableRefObject<HTMLButtonElement>;
+  spinnerColor?: string;
   children: React.ReactNode;
 }
--- frontend/src/components/common/Button/index.tsx
+++ frontend/src/components/common/Button/index.tsx
@@ -3,14 +3,14 @@ import { ButtonProps } from './button.props';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 import classNames from 'classnames';
 
-const Button: FC<ButtonProps> = ({ loading, ref, children, className, ...props }) => {
+const Button: FC<ButtonProps> = ({ loading, ref, spinnerColor, children, className, ...props }) => {
   return (
     <button
       ref={ref}
       className={classNames('button', className)}
       disabled={loading || props.disabled}
       {...props}>
-      {loading ? <LoadingSpinner size={17} /> : <>{children}</>}
+      {loading ? <LoadingSpinner color={spinnerColor || undefined} size={17} /> : <>{children}</>}
     </button>
   );
 };
