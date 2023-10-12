34e11b30a882341daf0b8ba7ad1cae76b83b6240
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-10-13 01:13:17 +0300

    categories redux slice

---
+++ frontend/src/redux/slices/categoriesSlice.ts
@@ -0,0 +1,92 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../store';
+import { ICategory } from '../../models/ICategory';
+import { AxiosResponse } from 'axios';
+import { AuthResponse } from '../../models/response/AuthResponse';
+import CategoriesService from '../../services/CategoriesService';
+import { Status } from '../../models/Status.enum';
+import { ErrorResponse } from '../../models/response/ErrorResponse';
+
+interface CategoriesState {
+  list: ICategory[];
+  status: string;
+  error: string;
+}
+
+const initialState: CategoriesState = {
+  list: null,
+  status: Status.SUCCESS,
+  error: null,
+};
+
+export const fetchCategories = createAsyncThunk<
+  AxiosResponse<ICategory[]>,
+  void,
+  { rejectValue: ErrorResponse }
+>('categories/get', async (_, { rejectWithValue }) => {
+  try {
+    const response = await CategoriesService.getCategories();
+    return response;
+  } catch (error) {
+    if (!error.response) {
+      throw error;
+    }
+    return rejectWithValue(error.response.data);
+  }
+});
+
+export const createCategory = createAsyncThunk<
+  AxiosResponse<ICategory[]>,
+  Partial<ICategory>,
+  { rejectValue: ErrorResponse }
+>('categories/create', async (params, { rejectWithValue }) => {
+  try {
+    const { name, parent } = params;
+    const response = await CategoriesService.createCategory(name, parent);
+    return response;
+  } catch (error) {
+    if (!error.response) {
+      throw error;
+    }
+    return rejectWithValue(error.response.data);
+  }
+});
+
+const filterSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // Запрос списка категорий
+    builder.addCase(fetchCategories.pending, (state, action) => {
+      state.status = Status.LOADING;
+      state.list = initialState.list;
+    });
+    builder.addCase(fetchCategories.fulfilled, (state, action) => {
+      state.status = Status.SUCCESS;
+      state.list = action.payload.data;
+    });
+    builder.addCase(fetchCategories.rejected, (state, action) => {
+      state.status = Status.ERROR;
+      state.error = action.payload.message;
+    });
+    // Запрос на создание категории
+    builder.addCase(createCategory.pending, (state, action) => {
+      state.status = Status.LOADING;
+    });
+    builder.addCase(createCategory.fulfilled, (state, action) => {
+      state.status = Status.SUCCESS;
+      state.list = action.payload.data;
+    });
+    builder.addCase(createCategory.rejected, (state, action) => {
+      state.status = Status.ERROR;
+      state.error = action.payload.message;
+    });
+  },
+});
+
+export const SelectCategoriesList = (state: RootState) => state.categories.list;
+export const SelectCategories = (state: RootState) => state.categories;
+export const {} = filterSlice.actions;
+
+export default filterSlice.reducer;
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 import filters from './slices/filterSlice';
 import auth from './slices/authSlice';
+import categories from './slices/categoriesSlice';
 
 export const store = configureStore({
   reducer: {
     filters,
     auth,
+    categories,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
