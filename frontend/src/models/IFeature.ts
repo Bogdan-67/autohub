3e53584b909282460d4750708acb238e898d55ea
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-10-15 01:54:37 +0300

    IGood and IFeature interfaces

---
+++ frontend/src/models/IFeature.ts
@@ -0,0 +1,6 @@
+export interface IFeature {
+  title: string;
+  id_feature: number;
+  good_id: number;
+  description: string;
+}
---
+++ frontend/src/models/IGood.ts
@@ -0,0 +1,14 @@
+import { IFeature } from './IFeature';
+
+export interface IGood {
+  id_good: number;
+  good_name: string;
+  article: string;
+  category_id: number;
+  brand_id: number;
+  price: number;
+  img: string | File;
+  description: string;
+  storage: number;
+  features?: Partial<IFeature>[];
+}
