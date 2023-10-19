c8bc8796ac78365944870ec4451e9dd04774844d
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-10-20 02:58:34 +0300

    fetchBrands in BrandsFilter

--- frontend/src/components/Catalog/Filter/BrandsFilter/index.tsx
+++ frontend/src/components/Catalog/Filter/BrandsFilter/index.tsx
@@ -1,20 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppDispatch } from '../../../../redux/store';
 import styles from './BrandsFilter.module.scss';
 import classNames from 'classnames';
 import { useSelector } from 'react-redux';
 import {
   SelectBrands,
+  SelectCategory,
   addBrand,
   clearBrands,
   removeBrand,
 } from '../../../../redux/slices/filterSlice';
-import brands from '../../../Main/BrandsSlider/brands.json';
 import FilterBlock from '../FilterBlock';
+import { useAppSelector } from '../../../../hooks/redux';
+import BrandService from '../../../../services/BrandService';
+import { IBrand } from '../../../../models/IBrand';
 
 const BrandsFilter = () => {
   const selectedBrands = useSelector(SelectBrands);
   const dispatch = useAppDispatch();
+  const category = useAppSelector(SelectCategory);
+  const [brands, setBrands] = useState<IBrand[]>([]);
 
   const handleClick = (id: number) => {
     if (selectedBrands.includes(id)) {
@@ -24,27 +29,44 @@ const BrandsFilter = () => {
     }
   };
 
+  const fetchBrands = async (categoryId: number) => {
+    await BrandService.getBrands(categoryId)
+      .then((response) => {
+        setBrands(response.data);
+      })
+      .catch((e) => console.log(e)); // TODO Сделать обработку ошибки
+  };
+
+  useEffect(() => {
+    fetchBrands(category);
+  }, [category]);
+
   return (
     <FilterBlock
       clearFunc={() => dispatch(clearBrands())}
-      title={'Брэнды'}
+      title={'Бренды'}
       isSelected={!!selectedBrands.length}>
-      <ul className={styles.brands__list}>
-        {brands.map((brand) => (
-          <li
-            key={brand.id}
-            className={classNames(styles.brand, {
-              [styles.active]: selectedBrands.includes(brand.id),
-            })}
-            onClick={() => handleClick(brand.id)}>
-            <span
-              className={classNames(styles.checkbox, {
-                [styles.checkbox_checked]: selectedBrands.includes(brand.id),
-              })}></span>
-            {brand.name}
-          </li>
-        ))}
-      </ul>
+      {/* TODO Добавить спиннер */}
+      {brands.length === 0 ? (
+        <div>Бренды не найдены</div>
+      ) : (
+        <ul className={styles.brands__list}>
+          {brands.map((brand) => (
+            <li
+              key={brand.id_brand}
+              className={classNames(styles.brand, {
+                [styles.active]: selectedBrands.includes(brand.id_brand),
+              })}
+              onClick={() => handleClick(brand.id_brand)}>
+              <span
+                className={classNames(styles.checkbox, {
+                  [styles.checkbox_checked]: selectedBrands.includes(brand.id_brand),
+                })}></span>
+              {brand.name}
+            </li>
+          ))}
+        </ul>
+      )}
     </FilterBlock>
   );
 };
