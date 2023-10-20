d3fd4ad9a5ce77e4b7f0bef8e45303d151940884
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-10-20 03:01:15 +0300

    getBrands query in BrandService

--- frontend/src/services/BrandService.ts
+++ frontend/src/services/BrandService.ts
@@ -3,7 +3,11 @@ import { AxiosResponse } from 'axios';
 import { IBrand } from '../models/IBrand';
 
 export default class BrandService {
-  static async createBrand(formdata: FormData): Promise<AxiosResponse<IBrand>> {
+  // FIXME: Понять почему не работает /brands
+  static getBrands(category_id?: number): Promise<AxiosResponse<IBrand[]>> {
+    return $api.get<IBrand[]>(`/brands-all${category_id ? `?category_id=${category_id}` : ''}`);
+  }
+  static createBrand(formdata: FormData): Promise<AxiosResponse<IBrand>> {
     return $api.post<IBrand>('/brands', formdata);
   }
 }
