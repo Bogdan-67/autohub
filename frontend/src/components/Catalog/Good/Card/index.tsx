8964fd14d4df0a574af0acf909d5e5ac2ccc901c
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-10-20 03:07:44 +0300

    fetchGoods in catalog

--- frontend/src/components/Catalog/Good/Card/index.tsx
+++ frontend/src/components/Catalog/Good/Card/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { FC } from 'react';
 import styles from './Card.module.scss';
+import { IGood } from '../../../../models/IGood';
+import { API_URL } from '../../../../http';
 
-const GoodCard = ({ img, title, price }) => {
+const GoodCard: FC<IGood> = ({ photos, good_name, id_good, price }) => {
   return (
     <div className={styles.card}>
-      <img className={styles.card__img} src={img} alt={''} />
-      <h4 className={styles.card__title}>{title}</h4>
+      <img
+        className={styles.card__img}
+        src={`${API_URL}/good-photos/${id_good}/${photos[0]}`}
+        alt={'Photo'}
+      />
+      <h4 className={styles.card__good_name}>{good_name}</h4>
       <p className={styles.card__price}>{price} ₽</p>
     </div>
   );
--- frontend/src/pages/User/Catalog/index.tsx
+++ frontend/src/pages/User/Catalog/index.tsx
@@ -1,25 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Catalog.module.scss';
 import Filter from '../../../components/Catalog/Filter';
 import TopSidebar from '../../../components/Catalog/TopSidebar';
 import { useAppSelector } from '../../../hooks/redux';
-import { SelectView } from '../../../redux/slices/filterSlice';
+import { SelectFilters, SelectView } from '../../../redux/slices/filterSlice';
 import GoodCard from '../../../components/Catalog/Good/Card';
 import GoodRow from '../../../components/Catalog/Good/Row';
 import sub from '../../../assets/sub.png';
 import Pagination from '../../../components/Catalog/Pagination';
+import GoodService from '../../../services/GoodService';
+import { IGood } from '../../../models/IGood';
 
 export const Catalog: React.FC = () => {
   const view = useAppSelector(SelectView);
   const [page, setPage] = useState<number>(1);
+  const { features, category } = useAppSelector(SelectFilters);
+  const [goods, setGoods] = useState<IGood[]>([]);
 
-  const goods = [...Array(12)].map((_, index) => {
-    return {
-      img: sub,
-      title: 'A10 Wide',
-      price: Math.floor(Math.random() * 100) * 1000,
-    };
-  });
+  const fetchGoods = async () => {
+    await GoodService.getGoods(category).then((response) => {
+      setGoods(response.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchGoods();
+  }, [category]);
 
   return (
     <section className={styles.wrapper}>
@@ -31,7 +37,7 @@ export const Catalog: React.FC = () => {
             if (view === 'cards') {
               return <GoodCard key={index} {...good} />;
             } else {
-              return <GoodRow key={index} />;
+              return <GoodRow key={index} {...good} />;
             }
           })}
         </div>
