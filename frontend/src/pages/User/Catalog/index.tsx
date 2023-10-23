a7af7c6896f09c70a6ee47aa0d08db00deaf8c3f
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-10-24 02:10:27 +0300

    сделал обработку загрузки и ошибок в каталоге

--- frontend/src/pages/User/Catalog/index.tsx
+++ frontend/src/pages/User/Catalog/index.tsx
@@ -3,29 +3,19 @@ import styles from './Catalog.module.scss';
 import Filter from '../../../components/Catalog/Filter';
 import TopSidebar from '../../../components/Catalog/TopSidebar';
 import { useAppSelector } from '../../../hooks/redux';
-import { SelectFilters, SelectView } from '../../../redux/slices/filterSlice';
+import { SelectView } from '../../../redux/slices/filterSlice';
 import GoodCard from '../../../components/Catalog/Good/Card';
 import GoodRow from '../../../components/Catalog/Good/Row';
-import sub from '../../../assets/sub.png';
 import Pagination from '../../../components/Catalog/Pagination';
-import GoodService from '../../../services/GoodService';
-import { IGood } from '../../../models/IGood';
+import { SelectGoodSlice } from '../../../redux/slices/goodSlice';
+import { Status } from '../../../models/Status.enum';
+import LoadingSpinner from '../../../components/common/LoadingSpinner/LoadingSpinner';
+import { Result } from 'antd';
 
 export const Catalog: React.FC = () => {
   const view = useAppSelector(SelectView);
   const [page, setPage] = useState<number>(1);
-  const { features, category } = useAppSelector(SelectFilters);
-  const [goods, setGoods] = useState<IGood[]>([]);
-
-  const fetchGoods = async () => {
-    await GoodService.getGoods(category).then((response) => {
-      setGoods(response.data);
-    });
-  };
-
-  useEffect(() => {
-    fetchGoods();
-  }, [category]);
+  const { status, goods, error } = useAppSelector(SelectGoodSlice);
 
   return (
     <section className={styles.wrapper}>
@@ -33,13 +23,30 @@ export const Catalog: React.FC = () => {
       <div className={styles.wrapper__content}>
         <TopSidebar />
         <div className={styles.wrapper__content__goods}>
-          {goods.map((good, index) => {
-            if (view === 'cards') {
-              return <GoodCard key={index} {...good} />;
-            } else {
-              return <GoodRow key={index} {...good} />;
-            }
-          })}
+          {status === Status.LOADING ? (
+            <div className={styles.wrapper__content__goods_loading}>
+              <LoadingSpinner size={30} color='#e8e8e8'></LoadingSpinner>
+            </div>
+          ) : status === Status.ERROR ? (
+            <div className={styles.wrapper__content__goods_loading}>
+              <Result
+                status='500'
+                title='Ошибка 500: Не удалось получить товары :('
+                subTitle={error}></Result>
+            </div>
+          ) : goods.length === 0 ? (
+            <div className={styles.wrapper__content__goods_loading}>Товары не найдены</div>
+          ) : (
+            <div className={styles.wrapper__content__goods__grid}>
+              {goods.map((good, index) => {
+                if (view === 'cards') {
+                  return <GoodCard key={index} {...good} />;
+                } else {
+                  return <GoodRow key={index} {...good} />;
+                }
+              })}
+            </div>
+          )}
         </div>
         <Pagination page={page} pageCount={7} handlePageClick={setPage} />
       </div>
